b6a471edcd86cfdb964654dc80d3cb325ce382c8
Richard Bamford <rbamfordz@gmail.com>
2019-11-19 10:54:32 +0000

    fixed missing then

--- gamemode/modules/rounds_system/sh_rounds.lua
+++ gamemode/modules/rounds_system/sh_rounds.lua
@@ -7,7 +7,7 @@ if SERVER then
 		if victim:Team() == TEAM_RUNNERS then -- switch the player to the other team
 			victim:SetTeam(TEAM_SPECTATOR)
 			victim:Spawn()
-		elseif victim:Team() == TEAM_DEATH
+		elseif victim:Team() == TEAM_DEATH then
 			victim:Spawn()
 		end
 
@@ -17,7 +17,7 @@ if SERVER then
 
 		if (runnersLeftAlive <= 1) then -- no more runners left alive, new round.
 			print("Winner is:", runners[1]:Nick())
-			
+
 			timer.Simple(5, function()
 				RunConsoleCommand("changelevel", game.GetMap())
 			end)
